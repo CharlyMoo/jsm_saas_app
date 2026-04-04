0f2a69980ffe366ea3271585650e8b90e8f59aae
cmoo <cmoo@frd.org.mx>
2026-04-04 13:21:28 -0600

    Add Navbar without dynamic content

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque, Geist } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
@@ -21,8 +22,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
-      <body className={`${bricolage.variable} antialiased`}>{children}</body>
+    <html lang="en">
+      <body className={`${bricolage.variable} antialiased`}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
