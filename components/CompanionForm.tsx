635df571568448a1e9c3760e030402b16ddc49ac
Charly Moo <charlymooayala@gmail.com>
2026-06-06 12:46:35 -0600

    Finish Form for building companion, agregate Select ui component

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -1,8 +1,8 @@
-"use client"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Controller, useForm } from "react-hook-form"
-import {z} from "zod"
-import {Button} from "@/components/ui/button"
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -10,64 +10,194 @@ import {
   FormField,
   FormItem,
   FormLabel,
-  FormMessage
-} from "@/components/ui/form"
-import {Input} from "@/components/ui/input"
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Select, SelectTrigger, SelectValue, SelectItem, SelectContent } from "./ui/select";
+import { subjects } from "@/constants";
+import { Textarea } from "./ui/textarea";
 
 const formSchema = z.object({
-    name: z.string().min(1, {message: 'Companion is required.'}),
-    subject: z.string().min(1, {message: 'Subject is required.'}),
-    topic: z.string().min(1, {message: 'Topic is required.'}),
-    voice: z.string().min(1, {message: 'Voice is required.'}),
-    style: z.string().min(1, {message: 'Style is required.'}),
-    duration: z.number().min(1, {message: 'Duration is required.'}),
-})
+  name: z.string().min(1, { message: "Companion is required." }),
+  subject: z.string().min(1, { message: "Subject is required." }),
+  topic: z.string().min(1, { message: "Topic is required." }),
+  voice: z.string().min(1, { message: "Voice is required." }),
+  style: z.string().min(1, { message: "Style is required." }),
+  duration: z.number().min(1, { message: "Duration is required." }),
+});
 
 const CompanionForm = () => {
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues:{
-      name: '',
-      subject: '',
-      topic: '',
-      voice: '',
-      style: '',
+    defaultValues: {
+      name: "",
+      subject: "",
+      topic: "",
+      voice: "",
+      style: "",
       duration: 15,
     },
-  })
+  });
 
   // 2. Define a submit handler
-  function onSubmit(values: z.infer<typeof formSchema>){
+  function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // This will be type safe
-    console.log(values)
+    console.log(values);
   }
-  
+
   return (
-        <Form {...form}>
-  <form onSubmit={form.handleSubmit(onSubmit)}>
-    
-    {/* 2. FormField hooks into a specific key in your Zod schema */}
-    <FormField
-      control={form.control}
-      name="name"
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>Username</FormLabel>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {/* 2. FormField hooks into a specific key in your Zod schema */}
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Companion name</FormLabel>
 
-          <FormControl>
-            <Input placeholder="shadcn" {...field} />
-          </FormControl>
-          <FormDescription>This is your public display name.</FormDescription>
-          <FormMessage />
-        </FormItem>
-      )}
-    />   
-    <Button type="submit">Submit</Button>
-  </form>
-</Form>
-  )
-}
+              <FormControl>
+                <Input
+                  placeholder="Enter the companion name"
+                  {...field}
+                  className="input"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="subject"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Subject</FormLabel>
 
-export default CompanionForm
+              <FormControl>
+                <Select onValueChange={field.onChange}
+                        value={field.value}
+                        defaultValue={field.value}>
+                  <SelectTrigger className="input capitalize">
+                    <SelectValue placeholder="Select the subject" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {subjects.map((subject) => (
+                      <SelectItem
+                          value={subject}
+                          key={subject}
+                          className="capitalize"
+                      >
+                        {subject}
+                      </SelectItem>
+
+                    ))}
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="topic"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>What should the companion help with?</FormLabel>
+
+              <FormControl>
+                <Textarea
+                  placeholder="Ex. Derivatives and Integrals"
+                  {...field}
+                  className="input"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="voice"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Voice</FormLabel>
+
+              <FormControl>
+                <Select onValueChange={field.onChange}
+                        value={field.value}
+                        defaultValue={field.value}>
+                  <SelectTrigger className="input">
+                    <SelectValue placeholder="Select the voice" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="male" >
+                      Male
+                    </SelectItem>
+                    <SelectItem value="female" >
+                      Female
+                    </SelectItem>
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="style"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Style</FormLabel>
+
+              <FormControl>
+                <Select onValueChange={field.onChange}
+                        value={field.value}
+                        defaultValue={field.value}>
+                  <SelectTrigger className="input">
+                    <SelectValue placeholder="Select the style"/>
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="formal" >
+                      Formal
+                    </SelectItem>
+                    <SelectItem value="casual" >
+                      Casual
+                    </SelectItem>
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="duration"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Estimated session duration in minutes</FormLabel>
+
+              <FormControl>
+                <Input
+                type="number"
+                  placeholder="15"
+                  {...field}
+                  className="input"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full cursor-pointer">Build Your Companion</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default CompanionForm;
