01ff080bcc99a4f8e1fa207b53ebd0c617ffe217
Charly Moo <charlymooayala@gmail.com>
2026-06-06 11:02:19 -0600

    Added CompanionForm.tsx

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -1,14 +1,72 @@
 "use client"
-
-import {maxLength, minLength, z} from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Controller, useForm } from "react-hook-form"
+import {z} from "zod"
+import {Button} from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form"
+import {Input} from "@/components/ui/input"
 
 const formSchema = z.object({
-    username: z.string().min(2).max(30),
+    name: z.string().min(1, {message: 'Companion is required.'}),
+    subject: z.string().min(1, {message: 'Subject is required.'}),
+    topic: z.string().min(1, {message: 'Topic is required.'}),
+    voice: z.string().min(1, {message: 'Voice is required.'}),
+    style: z.string().min(1, {message: 'Style is required.'}),
+    duration: z.number().min(1, {message: 'Duration is required.'}),
 })
 
 const CompanionForm = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues:{
+      name: '',
+      subject: '',
+      topic: '',
+      voice: '',
+      style: '',
+      duration: 15,
+    },
+  })
+
+  // 2. Define a submit handler
+  function onSubmit(values: z.infer<typeof formSchema>){
+    // Do something with the form values.
+    // This will be type safe
+    console.log(values)
+  }
+  
   return (
-    <div>Companion Form</div>
+        <Form {...form}>
+  <form onSubmit={form.handleSubmit(onSubmit)}>
+    
+    {/* 2. FormField hooks into a specific key in your Zod schema */}
+    <FormField
+      control={form.control}
+      name="name"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Username</FormLabel>
+
+          <FormControl>
+            <Input placeholder="shadcn" {...field} />
+          </FormControl>
+          <FormDescription>This is your public display name.</FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />   
+    <Button type="submit">Submit</Button>
+  </form>
+</Form>
   )
 }
 
